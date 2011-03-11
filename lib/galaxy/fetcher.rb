e14ed8a35bd43d53d4d7b00862ca0d44ea6e1997
Dain Sundstrom <dain@iq80.com>
2011-03-11 15:48:29 -0800

    Added support for maven snapshot versioning

--- lib/galaxy/fetcher.rb
+++ lib/galaxy/fetcher.rb
@@ -1,5 +1,7 @@
 require 'galaxy/temp'
 require 'galaxy/host'
+require 'galaxy/binary_version'
+require 'rexml/document'
 
 module Galaxy
     class Fetcher
@@ -8,7 +10,27 @@ module Galaxy
         end
 
         def fetch binary_version
-            core_url = "#{@base}/#{binary_version.repository_path}"
+            if binary_version.version =~ /SNAPSHOT/
+              require 'open-uri'
+              core_url =  "#{@base}/#{binary_version.group_id.gsub('.', '/')}/#{binary_version.artifact_id}/#{binary_version.version}/maven-metadata.xml"
+              contents = ""
+              open(core_url) do |io|
+                contents = io.read
+              end
+
+              maven_metadata = REXML::Document.new(contents)
+              snapshot_timestamp = REXML::XPath.first(maven_metadata, '/metadata/versioning/snapshot/timestamp').text
+              build_number = REXML::XPath.first(maven_metadata, '/metadata/versioning/snapshot/buildNumber').text
+              artifact_version = "#{binary_version.version.gsub('-SNAPSHOT', '')}-#{snapshot_timestamp}-#{build_number}"
+
+            else
+              artifact_version = binary_version.version
+            end
+
+            core_url = "#{@base}/#{binary_version.group_id.gsub('.', '/')}/#{binary_version.artifact_id}/#{binary_version.version}/#{binary_version.artifact_id}-#{artifact_version}"
+            core_url += "-#{binary_version.classifier}" unless binary_version.classifier.nil?
+            core_url += ".#{binary_version.packaging}"
+
             tmp_file = Galaxy::Temp.mk_auto_file "galaxy-download"
             @log.info "Fetching #{core_url} into #{tmp_file}"
             if @base =~ /^http:/
